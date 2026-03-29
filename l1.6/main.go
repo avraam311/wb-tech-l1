9721dfdcc2404110fac998e0618802f5fa090ca5
Ibragim <yandaroviba@mail.ru>
2026-03-29 13:11:40 +0300

    fix l1.6

--- l1.6/main.go
+++ l1.6/main.go
@@ -1,164 +1,124 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"sync"
-// )
-
-// func main() {
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-// 	go func(wg *sync.WaitGroup) {
-// 		defer wg.Done()
-// 		i := 0
-// 		for {
-// 			i++
-// 			fmt.Println("горутина работает")
-// 			// остановка горутины по условию
-// 			if i == 5 {
-// 				return
-// 			}
-// 		}
-// 	}(&wg)
-// 	wg.Wait()
-// }
-
-
-
-
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"sync"
-// 	"time"
-// )
-
-// func main() {
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-// 	quit := make(chan string)
-// 	go func(*sync.WaitGroup) {
-// 		defer wg.Done()
-// 		// выводим текст, пока канал quit не получит сообщение и горутина не будет завершена
-// 		for {
-// 			select {
-// 			case <-quit:
-// 				return
-// 			default:
-// 				time.Sleep(time.Second)
-// 				fmt.Println("горутина работает")
-// 			}
-
-// 		}
-// 	}(&wg)
-// 	time.Sleep(time.Second * 3)
-// 	quit <- "quit"
-// 	wg.Wait()
-// 	close(quit)
-// }
-
-
-
-
-
-// package main
-
-// import (
-// 	"context"
-// 	"fmt"
-// 	"sync"
-// 	"time"
-// )
-
-// func main() {
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	go func(*sync.WaitGroup) {
-// 		defer wg.Done()
-// 		// выводим текст, пока не откроется канал, который дает функция ctx.Done
-// 		for {
-// 			select {
-// 			case <-ctx.Done():
-// 				return
-// 			default:
-// 				time.Sleep(time.Second)
-// 				fmt.Println("горутина работает")
-// 			}
-// 		}
-// 	}(&wg)
-// 	time.Sleep(time.Second * 3)
-// 	//отменяем контекст
-// 	cancel()
-// 	wg.Wait()
-// }
-
-
-
-
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"runtime"
-// 	"sync"
-// 	"time"
-// )
-
-// func main() {
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-// 	go func(*sync.WaitGroup) {
-// 		defer wg.Done()
-// 		for {
-// 			time.Sleep(time.Second)
-// 			fmt.Println("горутина работает")
-// 			time.Sleep(time.Second)
-// 			fmt.Println("горутина работает")
-// 			// завершаем горутину, текст выводится только 2 раза
-// 			runtime.Goexit()
-// 			time.Sleep(time.Second)
-// 			fmt.Println("горутина работает")
-// 		}
-// 	}(&wg)
-// 	wg.Wait()
-// }
-
-
-
-
-
 package main
 
 import (
+	"context"
 	"fmt"
+	"runtime"
 	"sync"
-	// пакет с атомиками для предотвращения гонок горутин
 	"sync/atomic"
 	"time"
 )
 
-func main() {
-	// создаем флаг типа atomic.Bool, чтобы избежать гонки горутин
-	// хотя в данном коде это и не возможно, так как есть только 1 горутина
-	// даже если есть несколько горутин, они должны как-то влиять на флаг, чтобы нарушить работу
-	// но это хороший тон в любом случае
+func counterStop() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	stopCh := make(chan struct{})
+	go func() {
+		defer wg.Done()
+		i := 0
+		for {
+			i++
+			fmt.Println("горутина работает")
+			time.Sleep(time.Second)
+			if i == 5 {
+				stopCh <- struct{}{}
+				return
+			}
+		}
+	}()
+	<-stopCh
+	wg.Wait()
+}
+
+func channelStop() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	quit := make(chan string)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-quit:
+				return
+			default:
+				time.Sleep(time.Second)
+				fmt.Println("горутина работает")
+			}
+		}
+	}()
+	time.Sleep(time.Second * 3)
+	quit <- "quit"
+	wg.Wait()
+	close(quit)
+}
+
+func contextStop() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				time.Sleep(time.Second)
+				fmt.Println("горутина работает")
+			}
+		}
+	}()
+	time.Sleep(time.Second * 3)
+	cancel()
+	wg.Wait()
+}
+
+func goexitStop() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	stopCh := make(chan struct{})
+	go func() {
+		defer wg.Done()
+		for {
+			time.Sleep(time.Second)
+			fmt.Println("горутина работает")
+			stopCh <- struct{}{}
+			runtime.Goexit()
+		}
+	}()
+	<-stopCh
+	wg.Wait()
+}
+
+func atomicStop() {
 	var flag atomic.Bool
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go func(*sync.WaitGroup) {
+	go func() {
 		defer wg.Done()
-		// пока значение !flag.Load() не равно false, горутина работает
- 		for !flag.Load() {
+		for !flag.Load() {
 			time.Sleep(time.Second)
 			fmt.Println("горутина работает")
 		}
-	}(&wg)
+	}()
 	time.Sleep(time.Second * 3)
-	// через 3 секунды меняем значение флага и горутина завершает свою работу
 	flag.Store(true)
 	wg.Wait()
+}
+
+func main() {
+	fmt.Println("--- Метод 1: сигнал через канал после счётчика ---")
+	counterStop()
+
+	fmt.Println("\n--- Метод 2: канал quit ---")
+	channelStop()
+
+	fmt.Println("\n--- Метод 3: context cancel ---")
+	contextStop()
+
+	fmt.Println("\n--- Метод 4: runtime.Goexit ---")
+	goexitStop()
+
+	fmt.Println("\n--- Метод 5: atomic flag ---")
+	atomicStop()
 }
